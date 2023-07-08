a6d761d8a5bb89dc17f3fc7912809d493930c687
Aryn M Sparks <aryn.sparks1987@outlook.com>
2023-07-08 01:34:51 -0500

    Further improved code and documntation
    Completed readme

--- README.md
+++ README.md
@@ -1,6 +1,72 @@
-# libRangeMap
-A simple library  written in C++ that maps ranges of alphanumeric characters from one range to another.
+libRangeMap
+libRangeMap is a C++ library that provides a mapping functionality to map a given value within a certain range to a different range. It supports both numeric and character values, with characters being mapped to their respective positions in the alphabet.
 
-    To use, #include the header file and declare the namespace RANGEMAP.
-You will then be able to call the function MAP on alphanumeric charcters.
-The default output value of numeric values is a value between 0 and 1. English language characters are converted from 1-26 respectivly. Currently only the output of numeric values can be changed because I havent needed it to otherwise.
+Features
+Map a value within a specified input range to an output range.
+Support for both numeric and character values.
+Numeric values are linearly mapped to the output range.
+Character values are mapped to their respective positions in the alphabet.
+Getting Started
+Prerequisites
+C++ compiler with C++11 support.
+Usage
+Include the libRangeMap.h header file in your project.
+
+Create an instance of the RANGEMAP::RangeMapper class, specifying the input range, and optionally, the output range.
+// Example for mapping integers within the range [0, 100] to the range [0.0, 1.0]
+RANGEMAP::RangeMapper<int> mapper(0, 100, 0.0, 1.0);
+Use the map() function to map a value to the output range.
+// Map a value to the output range
+float mappedValue = mapper.map(50); // Example: Mapping the value 50
+You can also retrieve the input and output ranges using the getInputRange() and getOutputRange() functions.
+// Get the input range
+std::vector<int> inputRange = mapper.getInputRange();
+
+// Get the output range
+std::vector<float> outputRange = mapper.getOutputRange();
+// Get the input range
+std::vector<int> inputRange = mapper.getInputRange();
+
+// Get the output range
+std::vector<float> outputRange = mapper.getOutputRange();
+Examples
+Example 1: Mapping Integer Values
+#include "libRangeMap.h"
+#include <iostream>
+
+int main() {
+    // Create a RangeMapper instance to map integers within the range [0, 100] to the range [0.0, 1.0]
+    RANGEMAP::RangeMapper<int> mapper(0, 100, 0.0, 1.0);
+
+    // Map a value to the output range
+    float mappedValue = mapper.map(50); // Mapping the value 50
+
+    // Output the mapped value
+    std::cout << "Mapped Value: " << mappedValue << std::endl;
+
+    return 0;
+}
+Example 2: Mapping Character Values
+#include "libRangeMap.h"
+#include <iostream>
+
+int main() {
+    // Create a RangeMapper instance to map characters within the range ['A', 'Z'] to the range [0.0, 1.0]
+    RANGEMAP::RangeMapper<char> mapper('A', 'Z', 0.0, 1.0);
+
+    // Map a value to the output range
+    float mappedValue = mapper.map('D'); // Mapping the character 'D'
+
+    // Output the mapped value
+    std::cout << "Mapped Value: " << mappedValue << std::endl;
+
+    return 0;
+}
+License
+This project is licensed under the MIT License.
+
+Contributions
+Contributions to the libRangeMap library are welcome! If you find a bug, have a feature request, or want to contribute code improvements, please open an issue or submit a pull request on the GitHub repository.
+
+Acknowledgments
+The libRangeMap library was developed by Aryn M. Sparks as a personal project.
--- librangemap.h
+++ librangemap.h
@@ -1,103 +1,108 @@
-
-
 #ifndef LIBRANGEMAP_H_INCLUDED
 #define LIBRANGEMAP_H_INCLUDED
-#include <fstream>
-#include <type_traits>
+
 #include <stdexcept>
 #include <string>
 #include <vector>
-#include <iomanip>
+#include <sstream>
 #include <cctype>
 
 namespace RANGEMAP
 {
     /**
-     * The class MAP is used to map a given value of type T within a certain range
+     * The class RangeMapper is used to map a given value of type T within a certain range
      * to a different range (by default [0, 1]). It also handles both numeric and character values,
      * with characters being mapped to their respective positions in the alphabet.
      */
     template<class T>
-    class MAP
+    class RangeMapper
     {
-        public:
-            /**
-             * Constructor
-             * @param low - lower limit of input range
-             * @param high - higher limit of input range
-             * @param low_out - lower limit of output range (optional)
-             * @param high_out - higher limit of output range (optional)
-             */
-            MAP(T low, T high, float low_out = 0.0f, float high_out = 1.0f){
-                if(high <= low) {
-                    throw std::invalid_argument("High limit must be greater than low limit.");
-                }
-                if(high_out <= low_out) {
-                    throw std::invalid_argument("High output limit must be greater than low output limit.");
-                }
-
-                inRange.reserve(2);
-                outRange.reserve(2);
-                outRange.push_back(low_out);
-                outRange.push_back(high_out);
-
-                if(std::is_same<T,char>::value)
-                {
-                    inRange.push_back(convertToRange(low));
-                    inRange.push_back(convertToRange(high));
-                }
-                else
-                {
-                    inRange.push_back(low);
-                    inRange.push_back(high);
-                }
+    public:
+        /**
+         * Constructor
+         * @param low - lower limit of input range
+         * @param high - higher limit of input range
+         * @param low_out - lower limit of output range (optional)
+         * @param high_out - higher limit of output range (optional)
+         */
+        RangeMapper(T low, T high, float low_out = 0.0f, float high_out = 1.0f)
+        {
+            if (high <= low) {
+                throw std::invalid_argument("High limit must be greater than low limit.");
+            }
+            if (high_out <= low_out) {
+                throw std::invalid_argument("High output limit must be greater than low output limit.");
             }
 
-            /**
-             * Return the input range
-             * @return vector containing the input range
-             */
-            std::vector<T> getIRange(){
-                if(inRange.empty()){
-                    throw std::out_of_range("Input range is empty.");
-                }
-                return inRange;
+            inRange.reserve(2);
+            outRange.reserve(2);
+            outRange.push_back(low_out);
+            outRange.push_back(high_out);
+
+            if (std::is_same<T, char>::value)
+            {
+                inRange.push_back(convertToRange(low));
+                inRange.push_back(convertToRange(high));
             }
-
-            /**
-             * Return the output range
-             * @return vector containing the output range
-             */
-            std::vector<float> getORange(){
-                if(outRange.empty()){
-                    throw std::out_of_range("Output range is empty.");
-                }
-                return outRange;
+            else
+            {
+                inRange.push_back(low);
+                inRange.push_back(high);
             }
+        }
 
-            /**
-             * Map the given value to the output range
-             * @param toMap - value to map
-             * @return mapped value
-             */
-            float THIS(T toMap){
-                if(std::is_same<T,char>::value)
-                {
-                    toMap=convertToRange(toMap);
-                }
-                float derivitive =toMap/(inRange.at(1)-inRange.at(0));
-                float answer= outRange.at(0)+(outRange.at(1)-outRange.at(0))*derivitive;
+        /**
+         * Return the input range
+         * @return vector containing the input range
+         */
+        std::vector<T> getInputRange()
+        {
+            if (inRange.empty()) {
+                throw std::out_of_range("Input range is empty.");
+            }
+            return inRange;
+        }
 
-                if(answer > outRange.at(1)) { answer = outRange.at(1); }
-                if(answer < outRange.at(0)) { answer = outRange.at(0); }
-                return answer;
-            };
+        /**
+         * Return the output range
+         * @return vector containing the output range
+         */
+        std::vector<float> getOutputRange()
+        {
+            if (outRange.empty()) {
+                throw std::out_of_range("Output range is empty.");
+            }
+            return outRange;
+        }
+
+        /**
+         * Map the given value to the output range
+         * @param toMap - value to map
+         * @return mapped value
+         */
+        float map(T toMap)
+        {
+            if (std::is_same<T, char>::value)
+            {
+                toMap = convertToRange(toMap);
+            }
+            float derivative = toMap / (inRange.at(1) - inRange.at(0));
+            float answer = outRange.at(0) + (outRange.at(1) - outRange.at(0)) * derivative;
+
+            if (answer > outRange.at(1)) {
+                answer = outRange.at(1);
+            }
+            if (answer < outRange.at(0)) {
+                answer = outRange.at(0);
+            }
+            return answer;
+        };
 
     private:
         std::stringstream SS;
         std::vector<float> inRange;
         std::vector<float> outRange;
-        const std::string alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         /**
          * Converts char to numeric representation for a character (1-26 for A-Z), and 999 for other characters.
@@ -108,9 +113,9 @@ namespace RANGEMAP
         {
             char c = std::toupper(cvt);
             const int pos = static_cast<int>(alpha.find(c));
-            if(pos >=0 && pos <=25)
+            if (pos >= 0 && pos <= 25)
             {
-                return pos + 1 ;
+                return pos + 1;
             }
             else
             {
@@ -118,7 +123,6 @@ namespace RANGEMAP
             }
         }
     };
-
 };
 
 #endif // LIBRANGEMAP_H_INCLUDED
